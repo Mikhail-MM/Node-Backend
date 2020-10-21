685bc413be516a829828538bdf07c0cf0d1b5de3
Mikhail-MM <leakymee@bu.edu>
2020-10-21 03:14:14 -0400

    Added hyper quick notes on schema joining by hand

--- db/seeds/07_messages.js
+++ db/seeds/07_messages.js
@@ -6,7 +6,23 @@ const { getRandomInt } = require('../../utils/math');
 
 const numUsers = Number(process.env.SEED_NUM_USERS);
 const numChatRooms = Number(process.env.SEED_NUM_CHATROOMS);
-const numMessages = Number(process.env.SEED_NUM_MESSAGES);
+const numMessages = Number(process.env.SEED_NUM_MESSAGES_PER_ROOM);
+
+/*
+
+  TODO: Authenticated Seed
+  (IE Only participants will have messages in respective chat rooms);
+
+  const rooms = Array.from({ length: numChatRooms }, async (_, index) => {
+    let messageCount;
+    while (messageCount < numMessages) {
+
+    }
+  });
+  
+
+*/
+
 
 const payload = Array.from({ length: numMessages }, () => ({
   chatroom_id: getRandomInt(1, numChatRooms),
